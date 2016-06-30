1427a49ab2217555481039773330c3988b579522
Jason Lin <jason.lin.6086@gmail.com>
2016-06-29 17:29:12 -0700

    add new files

--- FlattenNestedListIterator/solution.cpp
+++ FlattenNestedListIterator/solution.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
